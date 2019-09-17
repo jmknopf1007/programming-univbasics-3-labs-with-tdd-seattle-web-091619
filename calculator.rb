dd1085af59697fb4b68ba66d18971ae410a8f6f8
Jacob Knopf <jmknopf1007@gmail.com>
2019-09-17 15:26:11 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -6,4 +6,8 @@ second_number = 3
 
 sum = first_number + second_number
 
-difference= first_number - second_number
+difference = first_number - second_number
+
+product = first_number * second_number
+
+quotient = first_number \ second_number
